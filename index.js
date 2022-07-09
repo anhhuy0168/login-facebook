8ef5ac442d2122381c7476f79826e3eedd404843
anhhuy0168 <anhhuy123zxc123@gmail.com>
2022-07-09 22:27:16 +0700

    change

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ passport.deserializeUser(function(user,done){
 
 
 app.get('/', (req, res) => {
-    res.send('<a href="/auth/facebook">Authenticate with facebook</a>');
+    res.send('<a href="/auth/facebook">Authenticate with facebookkkkk</a>');
   });
 
 app.get('/auth/facebook',
